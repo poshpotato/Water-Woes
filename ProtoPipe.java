44287359cd7a55b78e60e341efae5da8a32e1ed2
poshpotato <poshestpotato@gmail.com>
2021-05-18 12:22:01 +1200

    Set up ProtoType class without documentation.

---
+++ ProtoPipe.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class ProtoPipes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ProtoPipe
+{
+    // instance variables - replace the example below with your own
+    private int flowRate;
+    public ProtoPipe[] directions;
+
+    /**
+     * Constructor for objects of class ProtoPipes
+     */
+    public ProtoPipe(){}
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y){return y;}
+}
