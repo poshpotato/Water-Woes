b5226a519c383e41d607960b31848e61e8f5e82a
poshpotato <poshestpotato@gmail.com>
2021-06-15 12:17:27 +1200

    Graphics beginnings + end of the day commit

--- WWWindow.java
+++ WWWindow.java
@@ -18,7 +18,7 @@ import java.awt.event.*;
  * @Jebadiah Dudfield
  * @15/06/2021
  */
-public class WWWindow extends JFrame
+public class WWWindow extends JFrame implements ActionListener
 {
     JMenuBar menuBar;
     JMenu file;
@@ -67,22 +67,58 @@ public class WWWindow extends JFrame
         
         //Note: Savings hard!
         JMenuItem save = new JMenuItem("Save");
+        save.addActionListener(this);
         file.add(save);
         JMenuItem newNetw = new JMenuItem("New");
+        newNetw.addActionListener(this);
         file.add(newNetw);
         JMenuItem quit = new JMenuItem("Quit");
+        quit.addActionListener(this);
         file.add(quit);
         
         //Pipes menu. Changes selected Pipe.
         pipes = new JMenu("Pipes");
         menuBar.add(pipes);
         JMenuItem pipe = new JMenuItem("Pipe");
+        pipe.addActionListener(this);
         pipes.add(pipe);
         JMenuItem junction = new JMenuItem("Junction");
+        junction.addActionListener(this);
         pipes.add(junction);
         JMenuItem source = new JMenuItem("Source");
+        source.addActionListener(this);
         pipes.add(source);
         JMenuItem sink = new JMenuItem("Sink");
+        sink.addActionListener(this);
         pipes.add(sink);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        String cmd = e.getActionCommand();
+        //Switch statement to identify option selected.
+        switch(cmd){
+            case "Quit":
+                System.exit(0);
+                break;
+            case "Save":
+                ErrorReporter.reportError("Saving isn't currently implemented.");
+                break;
+            case "New":
+                ErrorReporter.reportError("TODO: Wire up pipe system.");
+                WWWindow l = new WWWindow();
+                break;
+            case "Red":
+                this.getContentPane().setBackground(Color.RED);
+                break;
+            case "Blue":
+                this.getContentPane().setBackground(Color.BLUE);
+                break;
+            case "Green":
+                this.getContentPane().setBackground(Color.GREEN);
+                break;
+            default:
+                ErrorReporter.reportError("Invalid Option \"" + cmd + "\".");
+                break;
+        }
     }
 }
