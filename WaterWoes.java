88fb650ffbe81164de2d953d85cbeaab6a601022
poshpotato <poshestpotato@gmail.com>
2021-07-20 11:50:05 +1200

    Added shell class with main method

---
+++ WaterWoes.java
@@ -0,0 +1,16 @@
+
+/**
+ * This is a shell class. all it does is create a Window.
+ *
+ * @Jebadiah Dudfield
+ * @20/07/2021
+ */
+public class WaterWoes
+{
+    private static WWWindow programWindow;
+    public static void main(String[] args)
+    {
+        //this is just ehre so I can try to pack it into a .jar.
+        programWindow = new WWWindow();
+    }
+}
